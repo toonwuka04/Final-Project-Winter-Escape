7994ea4b53be72240b3ce1285268c12e1451dc80
Tobechi <tobechionwuka@gmail.com>
2025-04-17 17:25:42 -0400

    Main Menu and folders created

---
+++ scripts/splash_screen_manager.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@export var load_scene : PackedScene
+@export var in_time: float = 0.5
+@export var fade_in_time: float = 1.5
+@export var pause_time : float = 1.5
+@export var fade_out_time : float = 1.5
+@export var out_time : float = 0.5
+@export var splash_screen : TextureRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	fade()
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+	
+func fade() -> void:
+	splash_screen.modulate.a = 0.0
+	var tween = self.create_tween()
+	tween.tween_interval(in_time)
+	tween.tween_property(splash_screen, "modulate:a", 1.0, fade_in_time)
+	tween.tween_interval(pause_time)
+	tween.tween_property(splash_screen, "modulate:a", 0.0, fade_out_time)
+	tween.tween_interval(out_time)
+	await tween.finished
+	get_tree().change_scene_to_packed(load_scene)
