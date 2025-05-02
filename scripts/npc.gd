16fdb32ee73bde23e720b44fdc68b9f3b90b8719
Tobechi <tobechionwuka@gmail.com>
2025-05-02 02:12:15 -0400

    added npc to level 1

---
+++ scripts/npc.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+#const SPEED = 300.0
+
+var is_chatting = false
+var player
+var player_is_in_chat = false
+
+
+func _ready() -> void:
+	pass
+	
+	
+func _process(delta: float) -> void:
+	$AnimatedSprite2D.play("idle")
+	
+	if Input.is_action_just_pressed("chat"):
+		$Dialogue.start()
+		#print("is chatting with npc")
+		is_chatting = true
+
+
+func _on_chat_detection_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player = body
+		player_is_in_chat = true
+		
+
+
+func _on_chat_detection_area_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		player_is_in_chat = false
+
+
+func _on_dialogue_dialogue_finished() -> void:
+	is_chatting = false
+	
